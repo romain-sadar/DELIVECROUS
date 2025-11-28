fc9796aa868cd58198e5a95cda31c59a3346acde
rmsdr <romainsadar.pro@gmail.com>
2025-11-28 14:47:58 +0100

    Add routes and split in dedicated modules

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
+import platsRouter from "../plats/plat.routes";
+import pointsRouter from "../points-livraison/point.routes";
+import commandesRouter from "../commandes/commande.routes";
 
 const router = Router();
 
 router.get("/", (_req, res) => {
   res.json({ message: "DeliveCROUS Livraison API" });
 });
+
+router.use("/plats", platsRouter);
+router.use("/points-livraison", pointsRouter);
+router.use("/commandes", commandesRouter);
 
 export default router;
